99dcc1d5841bf3c46ac0dda0b518ab7bae9a42dd
Zachary TN <104638755+ZacharyToyeNakamura@users.noreply.github.com>
2023-11-15 03:54:11 +0000

    finished q1 of exercise3.1

--- Exercises3.1FileReading/src/Main.java
+++ Exercises3.1FileReading/src/Main.java
@@ -1,114 +1,148 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
+/**
+ * x is rows
+ * y is columns
+ */
+
+
 public class Main {
-    public static final String FILENAME = "src/grid.txt";
-    public static final int[][] MOVES = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+    public static final String FILENAME = "/workspaces/javaG12/Exercises3.1FileReading/src/grid.txt";
+    public static final long[][] MOVES = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}}; 
     public static final int MN = 20;
-    public static int[][] grid = new int[MN][MN];
+    public static final int PATHLEN = 4;
+    public static long[][] grid = new long[MN][MN];
 
     public static void main(String[] args) throws FileNotFoundException {
-//        int grid[][] = new int[MN][MN];
         Scanner reader = new Scanner(new File(FILENAME));
         for(int i = 0; i < MN; i++) {
             for (int j = 0; j < MN; j++) {
                 if (reader.hasNextInt()) {
                     grid[i][j] = reader.nextInt();
                 } else {
-                    System.out.println("No int");
+                    System.out.println("No long");
                 }
             }
         }
-        int highest = 0;
-
-
-    }
-
-    public int dijkstra(int x, int y) {
-        PriorityQueue<pair> pq = new PriorityQueue<pair>(new pqComp());
-        int[][] high = new int[MN][MN]; // remove probably
-        for (int i = 0; i < MN; i++) {
-            for (int j = 0; j < MN; j++) {
-                high[i][j] = 0;
+        long highest = 0;
+        ArrayList<position> bestPath = new ArrayList<>();
+        ArrayList<position> tempPath = new ArrayList<>();
+        for(int i = 0; i < MN; i++) {
+            for(int j = 0; j < MN; j++) {
+                long test = dijkstra(i, j, tempPath);
+                if(test > highest) {
+                    highest = test;
+                    bestPath.clear();
+                    for(position p: tempPath) {
+                        bestPath.add(p.clone());
+                    }
+                }
             }
         }
+        System.out.println(highest);
+        System.out.println("The best path contains: ");
+        for(position posi: bestPath) {
+            System.out.println("("+posi.x+", "+posi.y+")");
+        }
+    }
 
-        ArrayList<position> temp = new ArrayList<>();
+    public static long dijkstra(int x, int y, ArrayList<position> bestPath) {
+        PriorityQueue<pair> pq = new PriorityQueue<pair>(new pqComp()); // create a pq with a custom compare
+        ArrayList<position> temp = new ArrayList<>(); // Array.asLit() . . .
         temp.add(new position(x, y));
-        pq.add(new pair(x, y, 0, grid[x][y], temp));
-        high[x][y] = 0;
+        pq.add(new pair(x, y, 1, grid[x][y], temp)); // init pq at starting pos
+        long highest = 0;
 
-        int highest = 0;
+        ArrayList<position> highPath = new ArrayList<>();
 
         while(!pq.isEmpty()) {
             pair next = pq.poll();
-            int xPos = next.pos.x;
-            int yPos = next.pos.y;
+            long xPos = next.pos.x;
+            long yPos = next.pos.y;
             ArrayList<position> vis = next.vis;
-            int val = next.val;
-            int dist = next.distance;
-            for(int[] move: MOVES) {
-                int cx = move[0], cy = move[1];
-                int x2 = cx + xPos, y2 = cy + yPos;
+            long val = next.val;
+            long dist = next.distance;
+
+            for(long[] move: MOVES) {
+                long cx = move[0], cy = move[1];
+                int x2 = (int)(cx + xPos), y2 = (int)(cy + yPos);
                 if(isIn(x2, y2, vis)) {
                     continue;
                 }
-                vis.add(new position(x2, y2));
-                val *= grid[x2][y2];
-                if(vis.size() >= 4) {
-                    if(highest < val) {
-                        highest = val;
+                // If out of bounds, don't continue;
+                if(x2 < 0 || x2 >= MN || y2 < 0 || y2 >= MN) continue;
+                ArrayList<position> nextVis = new ArrayList<>(vis);
+                nextVis.add(new position(x2, y2));
+                long nextVal = val * grid[x2][y2];
+                if(nextVis.size() >= PATHLEN) {
+                    if(highest < nextVal) {
+                        highest = nextVal;
+                        highPath = nextVis;
+                        
+                        // if(x == 0 && y == 0) {
+                        //     System.out.println("New high:");
+                        //     for (position posi : nextVis) {
+                        //         System.out.println("Including (" +posi.x + ", " + posi.y + ") == " + val);
+                        //     }
+                        // System.out.println("Visited size " + nextVis.size() + " distance " + dist);
+                        // }
                     }
                     continue;
                 }
-                pq.add(new pair(x, y, dist+1,val, vis));
-
+                pq.add(new pair(x2, y2, dist+1, nextVal, nextVis));
             }
-
-
+        }
+        bestPath.clear(); // Copy over the best path
+        for (position p : highPath) {
+            bestPath.add(p);
         }
         return highest;
     }
 
-    class position {
-        int x;
-        int y;
+    public static class position {
+        long x;
+        long y;
 
-        position(int x, int y) {
+        position(long x, long y) {
             this.x = x;
             this.y = y;
         }
+
+        /**
+         * Returns a duplicate of the position
+         */
+        public position clone() {
+            return new position(x, y);
+        }
     }
 
-    public class pair {
-        int distance;
-        ArrayList<position> vis;
+    public static class pair {
+        long distance; // This can be completely deleted
+        ArrayList<position> vis; // all past positions in the chain
+        position pos; // current position
+        long val; // current value
 
-        position pos;
-
-        int val;
-
-        pair(int x, int y, int dist, int val) {
+        pair(long x, long y, long dist, long val) {
             pos = new position(x, y);
             distance = dist;
             this.val = val;
             vis = new ArrayList<>();
         }
-        pair(int x, int y, int dist, int val, ArrayList<position> vis) {
-            this.pos.x = x;
-            this.pos.y = y;
+
+        pair(long x, long y, long dist, long val, ArrayList<position> vis) {
+            pos = new position(x, y);
             distance = dist;
             this.val = val;
             this.vis = vis;
         }
     }
 
-    class pqComp implements Comparator<pair>{
+    public static class pqComp implements Comparator<pair>{
         // lowest first
         public int compare(pair p1, pair p2) {
             if (p1.distance > p2.distance)
@@ -123,7 +157,7 @@ public class Main {
      * checks if a value is in a list 2d
      * @return
      */
-    public static boolean isIn(int x, int y, ArrayList<position> haystack) {
+    public static boolean isIn(long x, long y, ArrayList<position> haystack) {
         for (position pos: haystack) {
             if(x == pos.x && y == pos.y) {
                 return true;
