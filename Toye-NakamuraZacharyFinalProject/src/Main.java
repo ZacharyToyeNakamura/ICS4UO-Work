74035bab22f7513c1985306e565419d50d4da357
Zachary TN <zacharytoyenakamura@gmail.com>
2023-12-21 11:12:14 -0500

    looking at bits

--- Toye-NakamuraZacharyFinalProject/src/Main.java
+++ Toye-NakamuraZacharyFinalProject/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String str = "こんにちは、世界£©S~aÊ  ̳";
+        String result = "";
+        char[] messChar = str.toCharArray();
+
+        for (int i = 0; i < messChar.length; i++) {
+            result += ((String)(Integer.toBinaryString(messChar[i]))) + " ";
+        }
+
+        System.out.println(result);
     }
 }
--- Toye-NakamuraZacharyFinalProject/src/Store.java
+++ Toye-NakamuraZacharyFinalProject/src/Store.java
@@ -1,3 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Store {
-    
+    ArrayList<Item> inventory;
+
+    /**
+     * Initializes the array list
+     */
+    public Store() {
+        inventory = new ArrayList<>();
+    }
+
+
+    public boolean loadData(String filename) {
+        Scanner input;
+        try {
+            input = new Scanner(new File(filename));
+            while(input.hasNextLine()) {
+
+            }
+
+        } catch (IOException iox) {
+            System.out.println(iox.getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * Current Idea, convert the string to binary, then do something similar to base64 encryption except with 5 or 7
+     * bits at a time, include a shift mechanism
+     *
+     * @param str
+     * @param secret
+     * @return
+     */
+    public String encrypt(String str, int secret) {
+        final String ALPHA = "abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String output = "=";
+        Random rand = new Random(secret);
+        int shift = rand.nextInt(52);
+
+
+
+
+        return "";
+    }
+
+
 }
